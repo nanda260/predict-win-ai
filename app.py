4ae9e272b0ffafa17289a3594f3668995261fe30
Nanda Prihadi <nandaprihadi123@gmail.com>
2025-05-29 08:41:33 +0700

    m

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 model = joblib.load('model.pkl')
@@ -22,5 +23,6 @@ def predict():
 
     return render_template('index.html', prediction=prob_percent)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
